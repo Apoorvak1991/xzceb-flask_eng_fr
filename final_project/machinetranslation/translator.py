dbbbc5e2be30fc5543dfccc5b668fa8c2e697ed2
Apoorvak1991 <apoorvakailaswar@gmail.com>
2022-12-02 13:40:09 -0500

    Temp changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -6,19 +6,21 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-apikey = os.environ['apikey']
-url = os.environ['url']
+#apikey = os.environ['apikey']
+#url = os.environ['url']
 
-authenticator = IAMAuthenticator('apikey')
+authenticator = IAMAuthenticator('agdmm9bCE4yz047efGDuWB-LtxKkngRl3VhTdmXCDPp9')
 
 language_translator = LanguageTranslatorV3(version='2018-05-01',authenticator=authenticator)
-
-#language_translator.set_disable_ssl_verification(True)
-language_translator.set_service_url('url')
+language_translator.set_service_url('https://api.au-syd.language-translator.watson.cloud.ibm.com/instances/f80d248b-8b4d-4632-a985-c65dec9b8c6d')
 
 def english_to_french(englishText):
     translation = language_translator.translate(text=englishText,model_id='en-fr').get_result()
     frenchText=translation['translations'][0]['translation']
     return frenchText
 
-print(english_to_french("hello"))
+def french_to_english(frenchText):
+    translation = language_translator.translate(text=frenchText,model_id='fr-en').get_result()
+    englishText=translation['translations'][0]['translation']
+    return englishText
+
